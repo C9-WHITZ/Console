735c4a5f527e5e4694a35e6bb2ea9a2c46f4fb91
C9-WHITZ <142932816+C9-WHITZ@users.noreply.github.com>
2023-12-14 15:45:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+let nome = "Samuel ";
+let sobrenome = "Roque Pizzolatto Lima";
+
+let nomecompleto = nome.concat(sobrenome);
+console.log(nomecompleto);
+
+let quantidadecaracteres = nomecompleto.length;
+console.log(quantidadecaracteres);
+
+let irma = nomecompleto.replace("Samuel Roque", "Giovana Maia");
+console.log(irma);
+
+let a = Math.floor(Math.random()*100)
+console.log(a);
+
+let b = Math.floor(Math.random()*100)
+console.log(b);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
